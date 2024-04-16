31b5e999f50021625ac23172dd52efeed02f3d25
Gergely Horvath <horg3r@gmail.com>
2024-04-16 20:51:51 +0200

    fix: update php accessor script

--- bin/php.sh
+++ bin/php.sh
@@ -3,7 +3,7 @@
 current_dir=$(pwd)
 
 # Check if the current folder has a PHP Docker Compose service
-if docker compose ps | grep -qE 'app|php'; then
+if docker compose ps 2>/dev/null | grep -qE 'app|php'; then
     # Extract container name from docker-compose ps output
     container_name=$(docker compose ps | grep -E 'app|php' | awk '{print $1}')
     echo "Using container: $container_name"
@@ -11,6 +11,6 @@ if docker compose ps | grep -qE 'app|php'; then
     docker exec -ti "$container_name" bash -c "cd '$current_dir' && $*"
 else
     # Use default container name 'php'
-    echo "No PHP Docker Compose service found. Using default container 'php'."
+    echo "No PHP Docker Compose service found. Using default container 'reversebox-php'."
     docker exec -ti reversebox-php bash -c "cd '$current_dir' && $*"
 fi
